e9bfc070e38f7df6818b37bd85a375f0a82ba1ba
Pitura <pa.pitura@gmail.com>
2022-08-22 21:32:40 +0200

    audio on/off

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import VideoOnMobile from '../assets/video_mobile.mp4';
 import VideoOnDesktop from '../assets/video_desktop.mp4';
 import Countdown from "./Countdown";
+import Sound from "./Sound";
 
 function Main() {
 
@@ -16,17 +17,26 @@ function Main() {
         }
     }, []);
 
+
+    const [mute, setMute] = useState(true)
+
+    function LetMeHear() {
+        setMute(!mute);
+    }
+
     if (width >= 426) {
         return (
             <div className='main'>
-                <video src={VideoOnDesktop} autoPlay loop muted/>
+                <Sound props={LetMeHear}/>
+                <video src={VideoOnDesktop} autoPlay loop muted={mute}/>
                 <Countdown />
             </div>
         );
     } else {
         return (
             <div className='main'>
-                <video src={VideoOnMobile} autoPlay loop muted/>
+                <Sound props={LetMeHear}/>
+                <video src={VideoOnMobile} autoPlay loop muted={mute}/>
                 <Countdown />
             </div>
         );
---
+++ src/components/Sound.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+function Sound({props}) {
+
+    const [refresh, setRefresh] = useState(true)
+
+    function handleClick() {
+        if (typeof props === 'function') {
+            setRefresh(!refresh);
+            props();
+        }
+    }
+
+    if (refresh) {
+        return (
+            <div className='sound-icon-container'>
+                <i onClick={handleClick} className="fa-solid fa-volume-high sound-icons" ></i>
+            </div>
+        )
+    } else {
+        return (
+            <div className='sound-icon-container'>
+                <i onClick={handleClick} className="fa-solid fa-volume-xmark sound-icons"></i>
+            </div>
+        );
+    }
+}
+
+export default Sound;
